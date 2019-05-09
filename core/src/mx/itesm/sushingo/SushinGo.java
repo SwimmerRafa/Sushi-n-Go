1516a65b6c2f4381a2617006a2efa31e6cd9a339
Rafael Moreno <rafael_morenoc@outlook.com>
2019-05-09 14:17:32 -0500

    Commit 26 - Version casi FINAL

--- core/src/mx/itesm/sushingo/SushinGo.java
+++ core/src/mx/itesm/sushingo/SushinGo.java
@@ -2,15 +2,21 @@ package mx.itesm.sushingo;
 
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 
 
 public class SushinGo extends Game {
 	private final AssetManager assetManager = new AssetManager();
 
-	public void create() {setScreen(new TransitionScreen(this));}
+	public void create() {
+		Gdx.input.setCatchBackKey(true);
+		setScreen(new TransitionScreen(this));
+	}
+
 	public AssetManager getAssetManager() {
 		return assetManager;
 	}
+
 
 }
